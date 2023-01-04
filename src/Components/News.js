f6fa94827d6edf8464b7f4f12af33ba078a9bd50
Utsav Redekar <utsavredekar28@gmail.com>
2023-01-04 21:57:58 +0530

    inital commit

---
+++ src/Components/News.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner'
+import PropTypes from 'prop-types'
+
+
+export class News extends Component {
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category:"general"
+
+  }
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize:PropTypes.number,
+    category: PropTypes.string
+  }
+
+  constructor(){
+    super();
+    console.log("Hello I am constructor for news components")
+    this.state ={
+      articles : [],
+      loading: false,
+      page:1
+    }
+  }
+  async updateNews(pageNo){
+    const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fb0c7e7c3b7f490eb1dd0aadc899c304&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data= await fetch(url);
+    let parseData = await data.json() 
+    console.log(parseData);
+    this.setState({articles :parseData.articles,totalResults :parseData.totalResults, loading:false})
+
+
+  }
+  async componentDidMount(){
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fb0c7e7c3b7f490eb1dd0aadc899c304&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data= await fetch(url);
+    let parseData = await data.json()
+    console.log(parseData);
+    this.setState({articles :parseData.articles,totalResults :parseData.totalResults, loading:false})
+  }
+  
+  handlePrevClick = async ()=>{
+    {/*console.log("previous");
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fb0c7e7c3b7f490eb1dd0aadc899c304&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data= await fetch(url);
+    let parseData = await data.json()
+    console.log(parseData);
+    this.setState({
+      page:this.state.page - 1,
+      articles :parseData.articles,
+      loading: false
+    })*/}
+    this.setState({page: this.state.page - 1 })
+    this.updateNews();
+  }
+
+   handleNextClick = async()=>{
+    console.log("next");
+    {/*if(!(this.state.page + 1 >  Math.ceil(this.state.totalResults/this.props.pageSize ))){
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fb0c7e7c3b7f490eb1dd0aadc899c304&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+          this.setState({loading:true});
+        let data= await fetch(url);
+          let parseData = await data.json()
+          console.log(parseData);
+          this.setState({
+            page:this.state.page + 1,
+            articles :parseData.articles,
+            loading: false
+
+          })
+      }*/}
+      this.setState({page: this.state.page + 1 })
+      this.updateNews();
+   }
+  render() { 
+    return (
+      <div className='container my=3'>
+        <h1 className="text-center" style={{margin:'35px 0px'}}>NewsMonkey - Top Headlines</h1>
+          {this.state.loading &&<Spinner/>}
+        <div className="row">
+        {!this.state.loading && this.state.articles.map ((element)=>{
+          return(
+          <div className="col-md-4" key={element.url}>
+          <NewsItem  title={element.title?element.title :""} description ={element.description? element.description:""} imageUrl={element.urlToImage} newsUrl ={element.url} author ={element.author} date={element.publishedAt} source={element.source.name}/>
+          </div>
+          )})}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1}type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; previous</button>
+        <button disabled={this.state.page + 1 >  Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default News
